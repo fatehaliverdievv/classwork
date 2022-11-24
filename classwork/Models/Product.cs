b6346efb9472922d47cf204096fec0032b94ed08
User <tu7g0aq9h@code.edu.az>
2022-11-24 08:47:48 +0400

    classwork done

---
+++ classwork/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork.Models
+{
+    internal class Product:
+    {
+        string _name;
+        decimal _price;
+        Product[] product;
+
+        public Product()
+        {
+            product = new Product[0];
+        }
+        public void Add(Product productt)
+        {
+            Array.Resize(ref Product, product.Length + 1);
+            _student[_student.Length - 1] = por;
+        }
+
+
+
+    }
+}
---
+++ classwork/Models/User.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork.Models
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+        private bool _isSignedin;
+
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                value = value.Trim();
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (HasLower(value) && HasUpper(value) && HasDigit(value) && value.Length >= 8 && value.Length <= 25)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("wrong password");
+                }
+            }
+        }
+         
+        public bool Checkpassword(string password)
+        {
+            this.Password = password;
+            for (int i = 0; i < password.Length; i++)
+            {
+
+            if (HasLower(password) && HasUpper(password) && HasDigit(password) && password.Length >= 8 && password.Length <= 25)
+            {
+                    return true;
+            }
+            }
+            return false;
+        }
+
+
+
+
+
+        public bool HasDigit(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasUpper(string Password)
+        {
+            for (int i = 0; i < Password.Length; i++)
+            {
+                if (char.IsUpper(Password[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasLower(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool Issignedin
+        {
+            get
+        }
+
+
+    }
+}
